553f1664b42f7f9d15842ef7548608a7eebee39b
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2024-06-06 11:51:42 +0530

    Orders Page

--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -1,8 +1,120 @@
 import React from 'react'
 
+import "./Orders.scss"
 const Orders = () => {
+  const currentUser = {
+    id:1,
+    username: "john Doe",
+    isSeller:true
+  }
   return (
-    <div>Orders</div>
+    <div className="orders">
+<div className="container">
+  <div className="title">
+    <h1>Orders</h1>
+    
+  </div>
+  <table>
+    <tr>
+      <th>Image</th>
+      <th>Title</th>
+      <th>Price</th>
+      <th>{currentUser?.isSeller? "Buyer" : "Seller" }</th>
+      <th>Contacts</th>
+    </tr>
+    <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Stunning concept art</td>
+            <td>59.<sup>99</sup></td>
+            <td>Amrit gaur</td>
+            <td>
+              <img className="delete" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Ai generated concept art</td>
+            <td>120.<sup>99</sup></td>
+            <td>Kareena</td>
+            <td>
+              <img className="delete" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>High quality digital character</td>
+            <td>79.<sup>99</sup></td>
+            <td>John doe</td>
+            <td>
+              <img className="delete" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Illustration hyper realistic painting</td>
+            <td>119.<sup>99</sup></td>
+            <td>Hasifa</td>
+            <td>
+              <img className="delete" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Original ai generated digital art</td>
+            <td>59.<sup>99</sup></td>
+            <td>Amitabh</td>
+            <td>
+              <img className="delete" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Text based ai generated art</td>
+            <td>110.<sup>99</sup></td>
+            <td>Veera</td>
+            <td>
+              <img className="delete" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+  </table>
+</div>
+    </div>
   )
 }
 
